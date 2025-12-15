f2777400d12e77727faee79f94153d242f7e1fcb
atsyplenkov <atsyplenkov@fastmail.com>
2025-12-15 10:01:03 +0300

    force browsing data cleaning on Firefox closure. fix the arkenfox v140-1 release

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -17,10 +17,12 @@ user_pref("extensions.formautofill.creditCards.available", false);
 user_pref("extensions.formautofill.reauth.enabled", false);
 
 // Strengthen anti-fingerprinting
-user_pref("privacy.resistFingerprinting", false);
-user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.spoof_english", 2);
 user_pref("privacy.resistFingerprinting.reduceTimerPrecision.microseconds", 1000);
+
+// Disable fingerprinting resistance for a while
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
 
 // Canvas protections
 user_pref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", true);
@@ -32,4 +34,15 @@ user_pref("dom.webaudio.enabled", false);
 // Media device enumeration
 user_pref("media.navigator.enabled", false);
 
-// That's all folks!
+// Remove download history and cookies on exit
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", true);
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.downloads", true);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", true);
+
+// Don't close browser on the last tab closure
+user_pref("browser.tabs.closeWindowWithLastTab", false);

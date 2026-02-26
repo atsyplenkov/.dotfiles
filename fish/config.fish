91d4173857ac2edb7befd13e603655e60a82918f
atsyplenkov <sharp.couch9400@fastmail.com>
2026-02-26 09:19:23 +0300

    fish and ghostty configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,19 +2,41 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/ats/miniforge3/bin/conda
-    eval /home/ats/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/ats/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/ats/miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/ats/miniforge3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
+# CODEX CLI ---
+# For some reason I need to hardcode the home dir
+set -Ux CODEX_HOME $HOME/.codex
 
-# Aliases
+# Tirith ---
+# Protect catch ambigous characters
+# See more https://github.com/sheeki03/tirith
+tirith init --shell fish | source
+
+# Aliases ---
+## Default Claude Code
+alias cc='claude --settings ~/.claude/settings.json $argv'
+alias cc!='claude --settings ~/.claude/settings.json --permission-mode bypassPermissions $argv'
+
+## Claude Code with Z.AI models
 alias cz='claude --settings ~/.claude/settings-zai.json $argv'
 alias cz!='claude --settings ~/.claude/settings-zai.json --permission-mode bypassPermissions $argv'
+
+## Claude Code with Kimi models (via Ollama Cloud)
+alias ck='claude --settings ~/.claude/settings-kimi.json $argv'
+alias ck!='claude --settings ~/.claude/settings-kimi.json --permission-mode bypassPermissions $argv'
+
+## ls on steroids
+alias ll='ls -lha'
+
+## Internet speed
+alias csc='cloudflare-speed-cli'
+
+## Codex
+alias cx='codex $argv'
+alias cx!='codex --full-auto $argv'
+
+## Dolphin
+alias dp='dolphin . >/dev/null 2>&1 &; disown'
+
+# fzf ---
+# Set up fzf key bindings
+fzf --fish | source
